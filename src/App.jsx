43b727b34c3ac7e8efbfe8ba24200e4f0ea5d190
nell_aim <abiban0014@gmail.com>
2023-02-12 20:49:42 +0900

    基礎演習終了`

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
-const onClickButton = () => {
-  alert();
-};
-
 const App = () => {
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickShow = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() =>{
+    if(num > 0){
+      if(num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      }else{
+        faceShowFlag && setFaceShowFlag(false);
+      }};
+  },[num]
+  );
+
     return(
   <>
   <h1 style={{ color: `red`}}>Hello World !</h1>
@@ -15,7 +31,14 @@ const App = () => {
       <ColorfulMessage color = "pink">
       元気です
       </ColorfulMessage>
-  <button onClick={onClickButton}>button</button>
+  <button onClick={onClickCountUp}>count up</button>
+  <button onClick={onClickShow}>on/off</button>
+  <p>{ num }</p>
+  {faceShowFlag &&   <p>
+(´･ω･`)･ω･`)　ｷｬｰ<br/>
+/　　つ⊂　　＼　　ｺﾜｰｲ
+</p>}
+
   </>
     );
 };
